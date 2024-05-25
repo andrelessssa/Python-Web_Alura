f8973f1604c4f006e4b692740c8ffec540c5269e
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-05-25 00:32:06 -0300

    Autenticação
    Autenticação

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request, redirect, session
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -14,6 +14,7 @@ jogo4 = Jogo('Sonic', 'Aventura', 'PS4')
 lista = [jogo1, jogo2, jogo3, jogo4]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route('/')
 def inicio():    
@@ -21,16 +22,40 @@ def inicio():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=/novo')
+    
     return render_template('novo.html', titulo='Crie um novo Jogo:')
 
 @app.route('/criar', methods=['POST'])
 def criar():
+    
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
+    return redirect('/')
 
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    if 'aloha' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + 'Logado com Sucesso')
+        proxima_pagina = request.form['proxima']
+        return redirect('/{}'.format(proxima_pagina))
+    else:
+        flash('Usuario nao Logado')
+        return redirect('/login')
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum Usuario Logado')
+    return redirect('/')
 
 app.run(debug=True)
